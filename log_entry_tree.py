3a8ae9d08d7c7e8f324f5edeb7c997fac4933320
Jeff <abc@abc.com>
2023-03-20 22:01:22 -0400

    Subclass log entry tree.

---
+++ log_entry_tree.py
@@ -0,0 +1,11 @@
+from PyQt5 import uic, QtCore, QtGui, QtWidgets
+
+
+class CLogEntryTree(QtWidgets.QTreeWidget):
+  def __init__(self, parent):
+    super(CLogEntryTree, self).__init__(parent)
+    print('LogEntryTree created.')
+
+  def setLogDates(self, dateList):
+    # TODO: Implement
+    pass
