2c446f941cfb89d5ad0ac0be0101381dcca53905
Jeff <abc@abc.com>
2023-05-01 22:37:07 -0400

    Add FormatFlags class.

--- styleDef.py
+++ styleDef.py
@@ -14,12 +14,22 @@ class FormatFlag(IntFlag):
   Underline = 256
   Strikeout = 512
 
+class FormatFlags:
+  def __init__(self, initialFlags: set[FormatFlag]) -> None:
+    self.formatFlags: set[FormatFlag] = initialFlags
+
+  def hasFlag(self, flagToTest: FormatFlag) -> bool:
+    return flagToTest in self.formatFlags
+
+  def addFlag(self, flagToAdd: FormatFlag):
+    self.formatFlags.add(flagToAdd)
+
 class StyleDef:
   def __init__(self) -> None:
     self.strName = ''
     self.strDescription = ''
     self.strFontFamily = 'Helvetica'
-    self.fontPointSize = -1
+    self.fontPointSize = 10
     self.textColor = QtGui.QColor('black')
     self.backgroundColor = QtGui.QColor('white')
     self.bIsBold = False
@@ -27,14 +37,14 @@ class StyleDef:
     self.bIsUnderline = False
     self.bIsStrikeout = False
 
-    self.formatFlags = FormatFlag.NoFormat						# Don't set format
+    self.formatFlags = FormatFlags({ FormatFlag.NoFormat }) 						# Don't set format
 
   def setAllFormatFlags(self):
-    self.formatFlags = FormatFlag.FontFamily | \
-                        FormatFlag.FontSize | \
-                        FormatFlag.Bold | \
-                        FormatFlag.Italic | \
-                        FormatFlag.Underline | \
-                        FormatFlag.Strikeout | \
-                        FormatFlag.FGColorNone | \
-                        FormatFlag.BGColorNone
+    self.formatFlags = FormatFlags({ FormatFlag.FontFamily, \
+                                    FormatFlag.FontSize, \
+                                    FormatFlag.Bold, \
+                                    FormatFlag.Italic, \
+                                    FormatFlag.Underline, \
+                                    FormatFlag.Strikeout, \
+                                    FormatFlag.FGColorNone, \
+                                    FormatFlag.BGColorNone })
