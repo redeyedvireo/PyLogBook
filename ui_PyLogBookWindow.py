1f9dea0920f71b708c2499acc7ed525c22004841
Jeff Geraci <jeffg777@gmail.com>
2025-03-15 10:56:57 -0400

    Remove the tabbed widget.  The log browser and log editor are now always displayed.

--- ui_PyLogBookWindow.py
+++ ui_PyLogBookWindow.py
@@ -19,7 +19,7 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
 from PySide6.QtWidgets import (QApplication, QFormLayout, QHBoxLayout, QHeaderView,
     QLabel, QLineEdit, QMainWindow, QMenu,
     QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
-    QTabWidget, QTreeWidgetItem, QVBoxLayout, QWidget)
+    QTreeWidgetItem, QVBoxLayout, QWidget)
 
 from RichTextEdit import RichTextEditWidget
 from ccalendar import CCalendar
@@ -31,7 +31,7 @@ class Ui_PyLogBookWindow(object):
     def setupUi(self, PyLogBookWindow):
         if not PyLogBookWindow.objectName():
             PyLogBookWindow.setObjectName(u"PyLogBookWindow")
-        PyLogBookWindow.resize(824, 793)
+        PyLogBookWindow.resize(1264, 793)
         self.actionOpen = QAction(PyLogBookWindow)
         self.actionOpen.setObjectName(u"actionOpen")
         self.actionExit = QAction(PyLogBookWindow)
@@ -64,39 +64,61 @@ class Ui_PyLogBookWindow(object):
         __qtreewidgetitem.setText(0, u"Log Entries");
         self.logEntryTree.setHeaderItem(__qtreewidgetitem)
         self.logEntryTree.setObjectName(u"logEntryTree")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.logEntryTree.sizePolicy().hasHeightForWidth())
+        self.logEntryTree.setSizePolicy(sizePolicy)
         self.logEntryTree.setHeaderHidden(False)
 
         self.verticalLayout_3.addWidget(self.logEntryTree)
 
         self.curMonth = CCalendar(self.centralWidget)
         self.curMonth.setObjectName(u"curMonth")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.curMonth.sizePolicy().hasHeightForWidth())
-        self.curMonth.setSizePolicy(sizePolicy)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.curMonth.sizePolicy().hasHeightForWidth())
+        self.curMonth.setSizePolicy(sizePolicy1)
 
         self.verticalLayout_3.addWidget(self.curMonth)
 
 
         self.horizontalLayout_3.addLayout(self.verticalLayout_3)
 
-        self.tabWidget = QTabWidget(self.centralWidget)
-        self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setTabPosition(QTabWidget.TabPosition.South)
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.verticalLayout = QVBoxLayout(self.tab)
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setSpacing(6)
-        self.verticalLayout.setContentsMargins(11, 11, 11, 11)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label_2 = QLabel(self.centralWidget)
+        self.label_2.setObjectName(u"label_2")
+        font = QFont()
+        font.setPointSize(18)
+        self.label_2.setFont(font)
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.logBrowser = LogBrowser(self.centralWidget)
+        self.logBrowser.setObjectName(u"logBrowser")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(1)
+        sizePolicy2.setHeightForWidth(self.logBrowser.sizePolicy().hasHeightForWidth())
+        self.logBrowser.setSizePolicy(sizePolicy2)
+        self.verticalLayout_4 = QVBoxLayout(self.logBrowser)
+        self.verticalLayout_4.setSpacing(6)
+        self.verticalLayout_4.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+
+        self.verticalLayout.addWidget(self.logBrowser)
+
+
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setSpacing(6)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.label_3 = QLabel(self.tab)
+        self.label_3 = QLabel(self.centralWidget)
         self.label_3.setObjectName(u"label_3")
-        font = QFont()
-        font.setPointSize(18)
         self.label_3.setFont(font)
 
         self.verticalLayout_2.addWidget(self.label_3)
@@ -105,7 +127,7 @@ class Ui_PyLogBookWindow(object):
         self.formLayout.setSpacing(6)
         self.formLayout.setObjectName(u"formLayout")
         self.formLayout.setVerticalSpacing(2)
-        self.logDateLabel_2 = QLabel(self.tab)
+        self.logDateLabel_2 = QLabel(self.centralWidget)
         self.logDateLabel_2.setObjectName(u"logDateLabel_2")
         font1 = QFont()
         font1.setBold(True)
@@ -113,28 +135,28 @@ class Ui_PyLogBookWindow(object):
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.logDateLabel_2)
 
-        self.logDateLabel = QLabel(self.tab)
+        self.logDateLabel = QLabel(self.centralWidget)
         self.logDateLabel.setObjectName(u"logDateLabel")
         self.logDateLabel.setFont(font1)
 
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.logDateLabel)
 
-        self.lastModificationDateLabel_2 = QLabel(self.tab)
+        self.lastModificationDateLabel_2 = QLabel(self.centralWidget)
         self.lastModificationDateLabel_2.setObjectName(u"lastModificationDateLabel_2")
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.lastModificationDateLabel_2)
 
-        self.lastModificationDateLabel = QLabel(self.tab)
+        self.lastModificationDateLabel = QLabel(self.centralWidget)
         self.lastModificationDateLabel.setObjectName(u"lastModificationDateLabel")
 
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lastModificationDateLabel)
 
-        self.numChangesLabel_2 = QLabel(self.tab)
+        self.numChangesLabel_2 = QLabel(self.centralWidget)
         self.numChangesLabel_2.setObjectName(u"numChangesLabel_2")
 
         self.formLayout.setWidget(2, QFormLayout.LabelRole, self.numChangesLabel_2)
 
-        self.numChangesLabel = QLabel(self.tab)
+        self.numChangesLabel = QLabel(self.centralWidget)
         self.numChangesLabel.setObjectName(u"numChangesLabel")
 
         self.formLayout.setWidget(2, QFormLayout.FieldRole, self.numChangesLabel)
@@ -142,25 +164,25 @@ class Ui_PyLogBookWindow(object):
 
         self.verticalLayout_2.addLayout(self.formLayout)
 
-        self.logEdit = RichTextEditWidget(self.tab)
+        self.logEdit = RichTextEditWidget(self.centralWidget)
         self.logEdit.setObjectName(u"logEdit")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(1)
-        sizePolicy1.setHeightForWidth(self.logEdit.sizePolicy().hasHeightForWidth())
-        self.logEdit.setSizePolicy(sizePolicy1)
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(1)
+        sizePolicy3.setHeightForWidth(self.logEdit.sizePolicy().hasHeightForWidth())
+        self.logEdit.setSizePolicy(sizePolicy3)
 
         self.verticalLayout_2.addWidget(self.logEdit)
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setSpacing(6)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label = QLabel(self.tab)
+        self.label = QLabel(self.centralWidget)
         self.label.setObjectName(u"label")
 
         self.horizontalLayout_2.addWidget(self.label)
 
-        self.tagsEdit = QLineEdit(self.tab)
+        self.tagsEdit = QLineEdit(self.centralWidget)
         self.tagsEdit.setObjectName(u"tagsEdit")
 
         self.horizontalLayout_2.addWidget(self.tagsEdit)
@@ -171,7 +193,7 @@ class Ui_PyLogBookWindow(object):
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(6)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.addendumButton = QPushButton(self.tab)
+        self.addendumButton = QPushButton(self.centralWidget)
         self.addendumButton.setObjectName(u"addendumButton")
 
         self.horizontalLayout.addWidget(self.addendumButton)
@@ -180,7 +202,7 @@ class Ui_PyLogBookWindow(object):
 
         self.horizontalLayout.addItem(self.horizontalSpacer)
 
-        self.deleteButton = QPushButton(self.tab)
+        self.deleteButton = QPushButton(self.centralWidget)
         self.deleteButton.setObjectName(u"deleteButton")
 
         self.horizontalLayout.addWidget(self.deleteButton)
@@ -189,7 +211,7 @@ class Ui_PyLogBookWindow(object):
 
         self.horizontalLayout.addItem(self.horizontalSpacer_2)
 
-        self.submitButton = QPushButton(self.tab)
+        self.submitButton = QPushButton(self.centralWidget)
         self.submitButton.setObjectName(u"submitButton")
 
         self.horizontalLayout.addWidget(self.submitButton)
@@ -198,40 +220,12 @@ class Ui_PyLogBookWindow(object):
         self.verticalLayout_2.addLayout(self.horizontalLayout)
 
 
-        self.verticalLayout.addLayout(self.verticalLayout_2)
-
-        self.tabWidget.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.verticalLayout_6 = QVBoxLayout(self.tab_2)
-        self.verticalLayout_6.setSpacing(6)
-        self.verticalLayout_6.setContentsMargins(11, 11, 11, 11)
-        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
-        self.label_2 = QLabel(self.tab_2)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setFont(font)
-
-        self.verticalLayout_6.addWidget(self.label_2)
-
-        self.logBrowser = LogBrowser(self.tab_2)
-        self.logBrowser.setObjectName(u"logBrowser")
-        sizePolicy1.setHeightForWidth(self.logBrowser.sizePolicy().hasHeightForWidth())
-        self.logBrowser.setSizePolicy(sizePolicy1)
-        self.verticalLayout_4 = QVBoxLayout(self.logBrowser)
-        self.verticalLayout_4.setSpacing(6)
-        self.verticalLayout_4.setContentsMargins(11, 11, 11, 11)
-        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
-
-        self.verticalLayout_6.addWidget(self.logBrowser)
-
-        self.tabWidget.addTab(self.tab_2, "")
-
-        self.horizontalLayout_3.addWidget(self.tabWidget)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_2)
 
         PyLogBookWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QMenuBar(PyLogBookWindow)
         self.menuBar.setObjectName(u"menuBar")
-        self.menuBar.setGeometry(QRect(0, 0, 824, 33))
+        self.menuBar.setGeometry(QRect(0, 0, 1264, 33))
         self.menuFile = QMenu(self.menuBar)
         self.menuFile.setObjectName(u"menuFile")
         self.menuHelp = QMenu(self.menuBar)
@@ -257,9 +251,6 @@ class Ui_PyLogBookWindow(object):
 
         self.retranslateUi(PyLogBookWindow)
 
-        self.tabWidget.setCurrentIndex(0)
-
-
         QMetaObject.connectSlotsByName(PyLogBookWindow)
     # setupUi
 
@@ -274,6 +265,7 @@ class Ui_PyLogBookWindow(object):
         self.actionPreferences.setText(QCoreApplication.translate("PyLogBookWindow", u"Preferences...", None))
         self.actionExport_XML.setText(QCoreApplication.translate("PyLogBookWindow", u"Export XML...", None))
         self.actionImport_XML.setText(QCoreApplication.translate("PyLogBookWindow", u"Import XML...", None))
+        self.label_2.setText(QCoreApplication.translate("PyLogBookWindow", u"Log Browser", None))
         self.label_3.setText(QCoreApplication.translate("PyLogBookWindow", u"Log Editor", None))
         self.logDateLabel_2.setText(QCoreApplication.translate("PyLogBookWindow", u"Log Entry:", None))
         self.logDateLabel.setText(QCoreApplication.translate("PyLogBookWindow", u"-", None))
@@ -285,9 +277,6 @@ class Ui_PyLogBookWindow(object):
         self.addendumButton.setText(QCoreApplication.translate("PyLogBookWindow", u"Add Addendum", None))
         self.deleteButton.setText(QCoreApplication.translate("PyLogBookWindow", u"Delete Entry", None))
         self.submitButton.setText(QCoreApplication.translate("PyLogBookWindow", u"Save Entry", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("PyLogBookWindow", u"Editor", None))
-        self.label_2.setText(QCoreApplication.translate("PyLogBookWindow", u"Log Browser", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("PyLogBookWindow", u"Browser", None))
         self.menuFile.setTitle(QCoreApplication.translate("PyLogBookWindow", u"File", None))
         self.menuHelp.setTitle(QCoreApplication.translate("PyLogBookWindow", u"Help", None))
         self.menuSettings.setTitle(QCoreApplication.translate("PyLogBookWindow", u"Settings", None))
